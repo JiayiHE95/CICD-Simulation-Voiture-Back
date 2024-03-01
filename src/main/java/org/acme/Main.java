53c32e18cf2deef9fdf246d0b257e6cad0a0354e
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-03-01 11:23:49 +0100

    les fonctions

--- src/main/java/org/acme/Main.java
+++ src/main/java/org/acme/Main.java
@@ -1,9 +1,46 @@
 package org.acme;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String... args) throws InterruptedException {
-       Jeu jeu = new Jeu("Test");
-       jeu.animateTitle();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Entrez le nom de la voiture : ");
+        String nomVoiture = scanner.nextLine();
+
+        Voiture voiture = new Voiture(nomVoiture);
+
+        while (true) {
+            System.out.println("Que voulez-vous faire ?");
+            System.out.println("1. Avancer");
+            System.out.println("2. Reculer");
+            System.out.println("3. Monter");
+            System.out.println("4. Descendre");
+            System.out.println("5. Quitter");
+
+            int choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1:
+                    voiture.avancer();
+                    break;
+                case 2:
+                    voiture.reculer();
+                    break;
+                case 3:
+                    voiture.monter();
+                    break;
+                case 4:
+                    voiture.descendre();
+                    break;
+                case 5:
+                    System.out.println("Simulation terminée. Merci !");
+                    System.exit(0);
+                default:
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+            }
+
+        }
     }
 }
--- src/main/java/org/acme/Voiture.java
+++ src/main/java/org/acme/Voiture.java
@@ -1,9 +1,48 @@
 package org.acme;
 
+
+
 public class Voiture {
     private String nom;
+    private int positionX;
+    private int positionY;
 
     public Voiture(String nom) {
         this.nom = nom;
+        this.positionX = 0;
+        this.positionY = 0;
     }
+
+    public void avancer() {
+        positionX++;
+        int[] positions = getPosition();
+        System.out.println(nom + " avance. ("+ positions[0] + "," + positions[1] + ")");
+    }
+
+    public void reculer() {
+        positionX--;
+        int[] positions = getPosition();
+        System.out.println(nom + " recule. ("+ positions[0] + "," + positions[1] + ")");
+    }
+    // Ajoutez ces méthodes à votre classe Voiture
+    public void monter() {
+        positionY++;
+        int[] positions = getPosition();
+        System.out.println(nom + " monte. ("+ positions[0] + "," + positions[1] + ")");
+    }
+
+    public void descendre() {
+        positionY--;
+        int[] positions = getPosition();
+        System.out.println(nom + " descend. ("+ positions[0] + "," + positions[1] + ")");
+    }
+    public int[] getPosition() {
+        int[] positions = {positionX, positionY};
+        return positions;
+    }
+
+
+
 }
+
+
