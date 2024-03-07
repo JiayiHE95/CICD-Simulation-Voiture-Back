df752bc19ea732dd93da2b0d11eee45811807505
Jiayi HE <he_jia_yi1995@126.com>
2024-03-07 09:28:41 +0100

    recupere position

--- src/main/java/org/acme/Simulateur.java
+++ src/main/java/org/acme/Simulateur.java
@@ -2,15 +2,12 @@ package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
+import java.io.OutputStream;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-
-
 @ApplicationScoped
-public class Simulateur implements Runnable{
-
+public class Simulateur implements Runnable {
 
     private final Voiture voiture;
 
@@ -19,7 +16,6 @@ public class Simulateur implements Runnable{
     }
 
     private final static BlockingQueue<String> clientRequests = new LinkedBlockingQueue<>();
-
 
     @Inject
     public Simulateur() {
@@ -31,15 +27,17 @@ public class Simulateur implements Runnable{
         try {
             while (true) {
                 // Traitement des demandes clients
-                System.out.println(getClientRequests());
+                System.out.println("testetst" + getClientRequests());
                 String request = clientRequests.poll();
                 if (request != null) {
                     System.out.println("Simulateur received request: " + request);
                     processRequest(request);
+                    Thread.sleep(100);
+                } else {
+                    System.out.println("Simulateur received request: null");
+                    Thread.sleep(1000);
                 }
                 System.out.println(voiture.getPosition());
-                // Délai pour la simulation continue
-                Thread.sleep(1000); // ajustez selon vos besoins
             }
         } catch (InterruptedException e) {
             // Le thread a été interrompu (par exemple, à la fermeture de l'application)
@@ -49,21 +47,20 @@ public class Simulateur implements Runnable{
     void processRequest(String request) {
         // Logique pour traiter la demande du client
         System.out.println("Processing client request: " + request);
+        String position = null;
         switch (request) {
             case "avancer":
-                voiture.avancer();
-                System.out.println("avancer");
+                position = voiture.avancer();
                 break;
             case "reculer":
-                voiture.reculer();
+                position = voiture.reculer();
                 break;
             case "monter":
-                voiture.monter();
+                position = voiture.monter();
                 break;
             case "descendre":
-                voiture.descendre();
+                position = voiture.descendre();
                 break;
-            // Ajoutez d'autres cas en fonction de vos besoins
             default:
                 System.out.println("Commande invalide : " + request);
         }
--- src/main/java/org/acme/Voiture.java
+++ src/main/java/org/acme/Voiture.java
@@ -1,6 +1,5 @@
 package org.acme;
 
-
 public class Voiture {
     private final String nom;
     private int positionX;
@@ -12,32 +11,39 @@ public class Voiture {
         this.positionY = 0;
     }
 
-    public void avancer() {
+    public String avancer() {
         positionX++;
         System.out.println(nom + " avance." + getPosition());
+        return getPosition();
+
     }
 
-    public void reculer() {
-        positionX--;
+    public String reculer() {
+        if (positionX > 0) {
+            positionX--;
+        }
         System.out.println(nom + " recule" + getPosition());
+        return getPosition();
     }
+
     // Ajoutez ces méthodes à votre classe Voiture
-    public void monter() {
+    public String monter() {
         positionY++;
         System.out.println(nom + " monte. " + getPosition());
+        return getPosition();
     }
 
-    public void descendre() {
-        positionY--;
+    public String descendre() {
+        if (positionY > 0) {
+            positionY--;
+        }
         System.out.println(nom + " descend." + getPosition());
+        return getPosition();
     }
+
     public String getPosition() {
-        int[] positions = {positionX, positionY};
+        int[] positions = { positionX, positionY };
         return "(" + positions[0] + "," + positions[1] + ")";
     }
 
-
-
 }
-
-
